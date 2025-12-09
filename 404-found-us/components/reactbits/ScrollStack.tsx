0124ce3f296ab20fd608ca352404dcf725555efe
davidmortleman-beep <davidmortleman@gmail.com>
2025-12-09 21:12:50 +0300

    Fix TypeScript error: remove invalid smoothTouch option from Lenis config

--- 404-found-us/components/reactbits/ScrollStack.tsx
+++ 404-found-us/components/reactbits/ScrollStack.tsx
@@ -54,7 +54,6 @@ export default function ScrollStack({
         gestureOrientation: 'vertical',
         smoothWheel: true,
         wheelMultiplier: 1,
-        smoothTouch: false,
         touchMultiplier: 2,
         infinite: false,
       })
