7d62ecc696c0d66f8f7c755b7614ac495d871fa2
rodgnut-svg <rodgnut@gmail.com>
2025-12-24 14:15:40 +0000

    Delete 404 found us/.next/dev directory
    deleting because its under a new repo

--- 404 found us/.next/dev/static/chunks/adaf9_next_dist_client_components_builtin_global-error_9c9f90ce.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/adaf9_next_dist_3fafe9d1._.js"
-],
-    source: "dynamic"
-});
--- 404 found us/.next/dev/static/chunks/app_layout_tsx_78cdd4a3._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__7edfe6d4._.css"
-],
-    source: "dynamic"
-});
